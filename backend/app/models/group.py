a21cd04900d990d76e2679e673eef2814ee91a23
IanKlein6 <ianklein66@gmail.com>
2025-08-25 11:25:03 +0200

    test: changed pk to primary_key because of deprecation warning

--- backend/app/models/group.py
+++ backend/app/models/group.py
@@ -3,5 +3,5 @@ from tortoise import fields
 from tortoise.models import Model
 
 class Group(Model):
-    id = fields.IntField(pk=True)
+    id = fields.IntField(primary_key=True)
     name = fields.CharField(max_length=100, unique=True)
