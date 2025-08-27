e8d6070dcbe86f39e9f4a4c7fe87e9c47646e8db
IanKlein6 <ianklein66@gmail.com>
2025-08-27 20:35:28 +0200

    Refactor: Delete Student: added archiving to student model with soft-delete (archiving) restore (bring back from archive), hard delete (permanent delete) probably for either mistakes or after the 7 year minium of data retention.

--- backend/app/models/student.py
+++ backend/app/models/student.py
@@ -19,25 +19,38 @@ class Student(Model):
     first_name = fields.CharField(max_length=50)
     last_name = fields.CharField(max_length=50)
     email = fields.CharField(max_length=100, unique=True)
-    notes = fields.CharField(max_length=255, null=True)
+    notes = fields.TextField(null=True)
 
-    semester = fields.ForeignKeyField("models.Semester", related_name="students", null=True)
-    group = fields.ForeignKeyField("models.Group", related_name="students", null=True)
-    
+    semester = fields.ForeignKeyField("models.Semester", related_name="students", null=True, on_delete=fields.SET_NULL)
+    group = fields.ForeignKeyField("models.Group", related_name="students", null=True, on_delete=fields.SET_NULL)
 
     status = fields.CharEnumField(StudentStatus, default=StudentStatus.ACTIVE)
     attempt_num = fields.IntField(default=0)
 
     work_student_potential = fields.CharEnumField(WorkPotential, null=True)
     
-    archived_at = 
+    archived_at = fields.DatetimeField(null=True, index=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
+    @property # allows access like student.is_archived
+    def is_archived(self) -> bool:
+        return self.archived_at is not None # = True if archived_at has a value
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
     class Meta:
-        indexes = [
-            ("last_name", "first_name"), ("status",),
-            ]
+        indexes = [("last_name", "first_name"), ("status",),]
+
+class ArchiveAction(str, Enum):
+    ARCHIVE = "archive"
+    RESTORE= "restore"
+    HARD_DELETE = "hard_delete"
+
+class ArchiveLog(Model):
+    id = fields.IntField(primary_key=True)
+    student = fields.ForeignKeyField("models.Student", related_name="archive_logs", null=True, on_delete=fields.SET_NULL)
+    action = fields.CharEnumField(ArchiveAction)
+    timestamp = fields.DatetimeField(auto_now_add=True)
+    reason = fields.TextField(null=True)
